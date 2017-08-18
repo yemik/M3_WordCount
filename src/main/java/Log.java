fc9fc7a3683b3c41f65e29a1fd70f38f1f070c08
yemik <oduyemik@outlook.com>
2017-08-18 13:48:52 +0100

    Added Log class from previous project

---
+++ src/main/java/Log.java
@@ -0,0 +1,30 @@
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class Log {
+
+    final static String LOG_PROPERTIES_FILE = "resources/log4j.properties";
+    final static Logger logger = Logger.getLogger(Log.class.getName());
+
+    Log() {
+        initialiseLogging();
+    }
+
+    public static void initialiseLogging() {
+        PropertyConfigurator.configure(LOG_PROPERTIES_FILE);
+        logger.info("Logging initialised");
+    }
+
+    public void traceMessage(String message) {
+        logger.trace(message);
+    }
+
+    public void debugMessage(String message) {
+        logger.debug(message);
+    }
+
+    public void errorMessage(String message) {
+        logger.error(message);
+    }
+
+}
