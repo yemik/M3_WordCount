cef51ee056884bd88a98eee294aa5aaff01494a4
yemik <oduyemik@outlook.com>
2017-08-18 14:43:44 +0100

    Minor interface changes

--- src/main/java/FileHandling.java
+++ src/main/java/FileHandling.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.util.HashMap;
 
 public interface FileHandling {
+    String path = "";
     void readFile(File file);
     String readLine(File file);
     File[] splitFile(File largeFile);
