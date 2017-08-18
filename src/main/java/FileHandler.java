5afe1005914fcc75f97070edc01e1eb152abda44
yemik <oduyemik@outlook.com>
2017-08-18 14:44:42 +0100

    Beginning to implement FileHandling interface

--- src/main/java/FileHandler.java
+++ src/main/java/FileHandler.java
@@ -1,13 +1,25 @@
-import java.io.File;
+import java.io.*;
 
 public class FileHandler implements FileHandling {
+    private final String path;
+    String inputLine;
+
+    FileHandler(String path) {
+        this.path = path;
+    }
 
     public void readFile(File file) {
-
     }
 
     public String readLine(File file) {
-        return null;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+            while ((inputLine = reader.readLine()) != null) {
+                System.out.println(inputLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public File[] splitFile(File largeFile) {
